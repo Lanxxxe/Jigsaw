88cc511a062de192450977573e9146fc7dd2d797
Lanxxxe <Lanxxxe>
2024-12-15 13:46:48 +0800

    Initial Commit

---
+++ src/PuzzleGame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class PuzzleGame extends JFrame {
+     public PuzzleGame() {
+        this.dispose(); // Close the current window
+        new JLevelOne().setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            PuzzleGame game = new PuzzleGame();
+            game.setVisible(true);
+        });
+    }
+}
